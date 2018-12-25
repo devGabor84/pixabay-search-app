2482cacbbe1cbd8925eb4b7fd5fba4ec1b6abad9
Gabor Szephalmi <szephalmig@gmail.com>
2018-12-25 16:38:35 +0000

    item-model created

---
+++ src/app/search-page/item.model.ts
@@ -0,0 +1,11 @@
+export class Item {
+  constructor(
+    public src: string,
+    public title: string,
+    public description: string
+  ) {
+    this.src = src;
+    this.title = title;
+    this.description = description;
+  }
+}
